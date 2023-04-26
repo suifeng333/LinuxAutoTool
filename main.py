966858eb596e5ec6966477777f8ed4b83adbc20b
suifeng7663 <suifeng7663@outlook.com>
2023-04-26 16:36:34 +0800

    修复bug

--- main.py
+++ main.py
@@ -17,14 +17,13 @@ while (True):
     if (i==0):
         exit("您已退出！")
     if (i == 1):
-        i1 = int(input("选择操作系统：1.Ubuntu22.04LTS 2.Kali 0.退出"))
+        i1 = int(input("选择操作系统：1.Ubuntu22.04LTS 2.Kali 0.退出\n请输入："))
         if (i1 == 1):
             i2 = int(input("选择软件源：1.清华大学镜像源 2.北京外国语大学镜像源 3.中国科学技术大学镜像源 0.退出\n请输入："))
             if (i2 == 1):
                 print("WARN：操作过程中可能需要输入sudo密码")
                 os.system("sudo rm -rf /etc/apt/sources.list")
-                f = open('/etc/apt/' + 'sources' + '.list' + 'w+')
-                f.close()
+                os.system("sudo touch /etc/apt/sources.list")
                 f1 = open('/etc/apt/sources.list', 'w+')
                 f1.write(
                     "deb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse\ndeb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse\ndeb https://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse\ndeb http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse"
@@ -37,8 +36,7 @@ while (True):
             if (i2 == 2):
                 print("WARN：操作过程中可能需要输入sudo密码")
                 os.system("sudo rm -rf /etc/apt/sources.list")
-                f = open('/etc/apt/' + 'sources' + '.list' + 'w+')
-                f.close()
+                os.system("sudo touch /etc/apt/sources.list")
                 f1 = open('/etc/apt/sources.list', 'w+')
                 f1.write(
                     "deb https://mirrors.bfsu.edu.cn/ubuntu/ jammy main restricted universe multiverse\ndeb https://mirrors.bfsu.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse\ndeb https://mirrors.bfsu.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse\ndeb http://security.ubuntu.com/ubuntu/ jammy-security main restricted universe multiverse"
@@ -51,8 +49,7 @@ while (True):
             if (i2 == 3):
                 print("WARN：操作过程中可能需要输入sudo密码")
                 os.system("sudo rm -rf /etc/apt/sources.list")
-                f = open('/etc/apt/' + 'sources' + '.list' + 'w+')
-                f.close()
+                os.system("sudo touch /etc/apt/sources.list")
                 f1 = open('/etc/apt/sources.list', 'w+')
                 f1.write(
                     "deb https://mirrors.ustc.edu.cn/ubuntu/ jammy main restricted universe multiverse\ndeb https://mirrors.ustc.edu.cn/ubuntu/ jammy-security main restricted universe multiverse\ndeb https://mirrors.ustc.edu.cn/ubuntu/ jammy-updates main restricted universe multiverse\ndeb https://mirrors.ustc.edu.cn/ubuntu/ jammy-backports main restricted universe multiverse"
@@ -69,8 +66,7 @@ while (True):
             if (i2 == 1):
                 print("WARN：操作过程中可能需要输入sudo密码")
                 os.system("sudo rm -rf /etc/apt/sources.list")
-                f = open('/etc/apt/sources.list', 'w+')
-                f.close()
+                os.system("sudo touch /etc/apt/sources.list")
                 f1 = open('/etc/apt/sources.list', 'w+')
                 f1.write(
                     "deb https://mirrors.tuna.tsinghua.edu.cn/kali kali-rolling main non-free contrib"
@@ -83,8 +79,7 @@ while (True):
             if (i2 == 2):
                 print("WARN：操作过程中可能需要输入sudo密码")
                 os.system("sudo rm -rf /etc/apt/sources.list")
-                f = open('/etc/apt/' + 'sources' + '.list' + 'w+')
-                f.close()
+                os.system("sudo touch /etc/apt/sources.list")
                 f1 = open('/etc/apt/sources.list', 'w+')
                 f1.write(
                     "deb https://mirrors.bfsu.edu.cn/kali kali-rolling main non-free contrib"
@@ -97,8 +92,7 @@ while (True):
             if (i2 == 3):
                 print("WARN：操作过程中可能需要输入sudo密码")
                 os.system("sudo rm -rf /etc/apt/sources.list")
-                f = open('/etc/apt/' + 'sources' + '.list' + 'w+')
-                f.close()
+                os.system("sudo touch /etc/apt/sources.list")
                 f1 = open('/etc/apt/sources.list', 'w+')
                 f1.write(
                     "deb https://mirrors.ustc.edu.cn/kali kali-rolling main non-free contrib"
@@ -108,7 +102,9 @@ while (True):
                 print("正在更新软件源")
                 os.system("sudo apt update")
                 continue
-    if (i==2):
+    if (i == 2):
         print("WARN：操作过程中可能需要输入sudo密码")
         print("正在更新软件源并升级软件包")
         os.system("sudo apt update && upgrade")
+        print("已更新完成")
+        continue
